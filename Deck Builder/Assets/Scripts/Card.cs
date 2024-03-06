186e19761dec467561a6a883d8bfbb6cc59b3c29
EdenArts <ojedaeden08@gmail.com>
2024-03-05 18:14:18 -0800

    Changed the transform of the way the arrows were being spawned in

--- Deck Builder/Assets/Scripts/Card.cs
+++ Deck Builder/Assets/Scripts/Card.cs
@@ -17,22 +17,22 @@ public class Card : MonoBehaviour
         for(int i = 0; i < cardData.pattern.Count; i++) {
             if (cardData.pattern[i] == KeyCode.UpArrow)
             {
-                var upArrow = GameObject.Instantiate(Manager.instance.upArrow, this.transform.position, Quaternion.identity);
+                var upArrow = GameObject.Instantiate(Manager.instance.upArrow, this.transform.position, Manager.instance.upArrow.transform.rotation);
                 upArrow.transform.parent = this.transform.GetChild(0).transform;
             }
             else if (cardData.pattern[i] == KeyCode.DownArrow)
             {
-                var downArrow = GameObject.Instantiate(Manager.instance.downArrow, this.transform.position, Quaternion.identity);
+                var downArrow = GameObject.Instantiate(Manager.instance.downArrow, this.transform.position, Manager.instance.downArrow.transform.rotation);
                 downArrow.transform.parent = this.transform.GetChild(0).transform;
             }
             else if (cardData.pattern[i] == KeyCode.LeftArrow)
             {
-                var leftArrow = GameObject.Instantiate(Manager.instance.leftArrow, this.transform.position, Quaternion.identity);
+                var leftArrow = GameObject.Instantiate(Manager.instance.leftArrow, this.transform.position, Manager.instance.leftArrow.transform.rotation);
                 leftArrow.transform.parent = this.transform.GetChild(0).transform;
             }
             else if (cardData.pattern[i] == KeyCode.RightArrow)
             {
-                var rightArrow = GameObject.Instantiate(Manager.instance.rightArrow, this.transform.position, Quaternion.identity);
+                var rightArrow = GameObject.Instantiate(Manager.instance.rightArrow, this.transform.position, Manager.instance.rightArrow.transform.rotation);
                 rightArrow.transform.parent = this.transform.GetChild(0).transform;
             }
         }
